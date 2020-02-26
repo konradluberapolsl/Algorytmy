4bcc5b0a4e9c364e451e5c87b41542bf3c6061c8
Konrad <konrlub566@student.polsl.pl>
2020-02-26 18:07:31 +0100

    Initial commit Lab 1.

---
+++ Zad1.1.py
@@ -0,0 +1,18 @@
+def read(name):
+    file = open(name, "r")
+    numbers = file.read().splitlines()
+    file.close()
+    print(numbers)
+    return numbers
+
+def find_max(numbers):
+    max = 0.0
+    for i in range(len(numbers)):
+        if i == 0: max = float(numbers[i])
+        if float(numbers[i])>max: max = float(numbers[i])
+        print(max)
+
+
+
+
+find_max(read("Zad1.1.txt"))
